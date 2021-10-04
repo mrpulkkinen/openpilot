e7853fc8cf29d3724a4a370e6cee922e86a33e2c
mrpulkkinen <pulken@gmail.com>
2021-10-04 12:28:35 +0200

    Updated QR tag to include IMEI and Serial
    Removed Comma.ai and included retropilot.api
    
    https://github.com/Circuit-Pro/openpilot/commit/ec840ae3b47267b04ae7a99686b93f55f13d22a8#diff-3faca5e7de50cb3da1c28a9521769584f802a7b9b7f515deac9f4318a94d4d93L36

--- selfdrive/ui/qt/widgets/prime.cc
+++ selfdrive/ui/qt/widgets/prime.cc
@@ -31,8 +31,18 @@ void PairingQRWidget::showEvent(QShowEvent *event) {
 }
 
 void PairingQRWidget::refresh() {
+  Params params;
+  QString IMEI = QString::fromStdString(params.get("IMEI"));
+  QString serial = QString::fromStdString(params.get("HardwareSerial"));
+
+  if (std::min(IMEI.length(), serial.length()) <= 5) {
+    qrCode->setText("Error getting serial: contact support");
+    qrCode->setWordWrap(true);
+    qrCode->setStyleSheet(R"(font-size: 60px;)");
+    return;
+  }
   QString pairToken = CommaApi::create_jwt({{"pair", true}});
-  QString qrString = "https://connect.comma.ai/?pair=" + pairToken;
+  QString qrString = IMEI + "--" + serial + "--" + pairToken;
   this->updateQrCode(qrString);
 }
 
