b86cc2b312d03a0ec96cdb912633249564e1e64f
mrpulkkinen <pulken@gmail.com>
2021-10-24 10:42:48 +0200

    Revert "merged with actuallylemoncurd"
    This reverts commit 34bffbf9b600cddb66ad3ca9b0315fff878c7eb8, reversing
    changes made to 70f540a86986938c98c3a8e12682948e120f2b27.

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -87,8 +87,8 @@ def only_toyota_left(candidate_cars):
 
 # **** for use live only ****
 def fingerprint(logcan, sendcan):
-  fixed_fingerprint = "VOLKSWAGEN GOLF 6TH GEN"
-  skip_fw_query = True
+  fixed_fingerprint = os.environ.get('FINGERPRINT', "")
+  skip_fw_query = os.environ.get('SKIP_FW_QUERY', False)
 
   if not fixed_fingerprint and not skip_fw_query:
     # Vin query only reliably works thorugh OBDII
